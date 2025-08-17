90c6c036c669a65b3ae056010909d66076f798b6
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-17 20:37:15 +0300

    js

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // ناخد كل الـ arguments بعد مسار الملف
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
