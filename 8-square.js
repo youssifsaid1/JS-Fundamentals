54d091fbf78f2ee3a53cea9beac6c96436322450
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-17 20:53:44 +0300

    js

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+    console.log("Missing size");
+} else {
+    let i = 0;
+    while (i < size) {
+        console.log("X".repeat(size));
+        i++;
+    }
+}
